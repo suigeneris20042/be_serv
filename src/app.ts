3e7ab600b6b8f733a61f936e6d4de865f7e9ad1d
FrancisHZWM <francis.huamannahui@unsaac.edu.pe>
2024-11-26 23:16:05 -0500

    revisado api

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';  // Importar cors
-import { roleRoutes } from './routes/roleRoutes';  // Importar rutas de roles
-import userRoutes from './routes/userRoutes';  // Rutas de usuarios
-import authRoutes from './routes/authRoutes';  // Rutas de autenticación
-import bienRoutes from './routes/bienRoutes';  // Rutas de bienes
-import servicioRoutes from './routes/servicioRoutes';  // Rutas de servicios
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors"; // Importar cors
+import { roleRoutes } from "./routes/roleRoutes"; // Importar rutas de roles
+import userRoutes from "./routes/userRoutes"; // Rutas de usuarios
+import authRoutes from "./routes/authRoutes"; // Rutas de autenticación
+import bienRoutes from "./routes/bienRoutes"; // Rutas de bienes
+import servicioRoutes from "./routes/servicioRoutes"; // Rutas de servicios
 
 const app = express();
 
 // Configuración de CORS para permitir solo solicitudes desde un dominio específico
 const corsOptions = {
-  origin: 'http://localhost:3000',  // Reemplaza con el dominio de tu página web
-  optionsSuccessStatus: 200        // Para asegurar compatibilidad con algunos navegadores antiguos
+  origin: "http://localhost:3002", // Reemplaza con el dominio de tu página web
+  optionsSuccessStatus: 200, // Para asegurar compatibilidad con algunos navegadores antiguos
 };
 
 // Aplicar CORS globalmente a todas las rutas
@@ -22,18 +22,21 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // Usar las rutas
-app.use('/api/roles', roleRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/bienes', bienRoutes);
-app.use('/api/servicios', servicioRoutes);
+app.use("/api/roles", roleRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/bienes", bienRoutes);
+app.use("/api/servicios", servicioRoutes);
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bd_webservicios')
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch((err) => console.log('Error al conectar a MongoDB:', err));
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/bd_webservicios"
+  )
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.log("Error al conectar a MongoDB:", err));
 
 // Iniciar servidor
 app.listen(3001, () => {
-  console.log('Servidor corriendo en el puerto 3001');
+  console.log("Servidor corriendo en el puerto 3001");
 });
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from 'express';
-import User from '../models/userModel';
+import { Request, Response } from "express";
+import User from "../models/userModel";
 
 export const getAllUsers = async (req: Request, res: Response) => {
   const users = await User.find();
@@ -10,26 +10,30 @@ export const createUser = async (req: Request, res: Response) => {
   try {
     const newUser = new User(req.body);
     await newUser.save();
-    res.status(201).json(newUser);
+    res
+      .status(201)
+      .json({ newUser, message: "Usuario registrado exitosamente!" });
   } catch (err) {
-    res.status(400).json({ message: (err as Error).message });
+    res.status(400).json({ message: "El nombre de usuario ya esta en uso" });
   }
 };
 
 export const getUserById = async (req: Request, res: Response) => {
   const user = await User.findById(req.params.id);
-  if (!user) return res.status(404).json({ message: 'Usuario no encontrado' });
+  if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
   res.json(user);
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  if (!user) return res.status(404).json({ message: 'Usuario no encontrado' });
-  res.json(user);
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+  res.json({ user, message: "Usuario Actualizado" });
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
   const user = await User.findByIdAndDelete(req.params.id);
-  if (!user) return res.status(404).json({ message: 'Usuario no encontrado' });
-  res.json({ message: 'Usuario eliminado' });
+  if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
+  res.json({ message: "Usuario eliminado" });
 };
